c612803af76f5f7ab1dcad99495bf0dab868e5b2
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-28 14:49:39 +0800

    Update halo.sh

--- halo.sh
+++ halo.sh
@@ -1,1 +1,74 @@
+#!/bin/bash
 
+# 检查是否已安装 Docker 和 Docker Compose
+command -v docker >/dev/null 2>&1 || { echo >&2 "请先安装 Docker！"; exit 1; }
+command -v docker-compose >/dev/null 2>&1 || { echo >&2 "请先安装 Docker Compose！"; exit 1; }
+
+# 创建 WordPress 目录
+mkdir ~/halo && cd ~/halo
+
+# 获取用户输入的端口号
+echo -n "请输入要映射的端口号（例如：8080）："
+read port
+
+# 创建 Docker Compose 配置文件
+cat > docker-compose.yml <<EOF
+version: "3"
+
+services:
+  halo:
+    image: halohub/halo:2.11
+    container_name: halo
+    restart: on-failure:3
+    depends_on:
+      halodb:
+        condition: service_healthy
+    networks:
+      halo_network:
+    volumes:
+      - ./halo2:/root/.halo2
+    ports:
+      - "8090:8090"
+    healthcheck:
+      test: ["CMD", "curl", "-f", "http://localhost:8090/actuator/health/readiness"]
+      interval: 30s
+      timeout: 5s
+      retries: 5
+      start_period: 30s          
+    command:
+      - --spring.r2dbc.url=r2dbc:pool:postgresql://halodb/halo
+      - --spring.r2dbc.username=halo
+      # PostgreSQL 的密码，请保证与下方 POSTGRES_PASSWORD 的变量值一致。
+      - --spring.r2dbc.password=openpostgresql
+      - --spring.sql.init.platform=postgresql
+      # 外部访问地址，请根据实际需要修改
+      - --halo.external-url=http://localhost:8090/
+  halodb:
+    image: postgres:15.4
+    container_name: halodb
+    restart: on-failure:3
+    networks:
+      halo_network:
+    volumes:
+      - ./db:/var/lib/postgresql/data
+    ports:
+      - "5432:5432"
+    healthcheck:
+      test: [ "CMD", "pg_isready" ]
+      interval: 10s
+      timeout: 5s
+      retries: 5
+    environment:
+      - POSTGRES_PASSWORD=openpostgresql
+      - POSTGRES_USER=halo
+      - POSTGRES_DB=halo
+      - PGUSER=halo
+
+networks:
+  halo_network:
+EOF
+
+# 启动 WordPress
+docker-compose up -d
+
+echo "Halo 已成功部署！"
