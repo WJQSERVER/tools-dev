409780a6422d0167e186a9de3b3fb6bdd665b9b6
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-29 09:53:27 +0800

    Update uptime.sh

--- uptime.sh
+++ uptime.sh
@@ -7,7 +7,7 @@ if ! command -v docker >/dev/null || ! command -v docker-compose >/dev/null; the
 fi
 
 # 创建数据存储目录
-data_dir="/root/data/docker_data/npm"
+data_dir="/root/data/docker_data/uptime"
 mkdir -p "$data_dir"
 
 # 提示用户输入容器端口
@@ -15,7 +15,7 @@ read -p "请输入要映射到 Uptime Kuma 容器的端口（默认为 3000）: 
 container_port=${container_port:-3000}
 
 # 创建 Docker Compose 配置文件
-cat > docker-compose.yml <<EOF
+cat > "$data_dir/docker-compose.yml" <<EOF
 version: '3.8'
 services:
   uptime-kuma:
@@ -26,6 +26,9 @@ services:
       - $data_dir:/var/lib/uptime-kuma
     restart: always
 EOF
+
+# 切换到数据存储目录
+cd "$data_dir" || exit
 
 # 启动容器
 docker-compose up -d
