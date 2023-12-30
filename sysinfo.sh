5e9f4cba7a4e194cc3d92ab32ea5c93c40193298
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2023-12-30 08:20:26 +0800

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,22 +1,67 @@
 #!/bin/bash
 
-# 获取系统信息
-kernel_version=$(uname -r)
+# 获取主机名
 hostname=$(hostname)
-uptime=$(uptime -p)
-os=$(lsb_release -d | cut -f 2-)
-cpu=$(grep -m1 'model name' /proc/cpuinfo | cut -d ':' -f 2 | sed 's/^[ \t]*//')
-memory=$(free -h | awk '/Mem/ {print $2}')
-disk=$(df -h --total | awk '/total/ {print $2}')
 
-# 打印系统概况
-echo "系统概况"
-echo "-------------------------------------"
+# 获取运营商信息
+isp_info=$(curl -s https://ipinfo.io/org)
+isp=$(echo "$isp_info" | awk '{split($0,a," "); print a[2]}')
+
+# 获取Linux版本
+linux_version=$(uname -r)
+
+# 获取CPU架构
+cpu_arch=$(uname -m)
+
+# 获取CPU型号
+cpu_model=$(lscpu | grep "Model name:" | awk -F: '{print $2}' | awk '{$1=$1;print}')
+
+# 获取CPU核心数
+cpu_cores=$(nproc)
+
+# 获取CPU占用率
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
+
+# 获取物理内存使用情况
+physical_memory=$(free -m | grep "Mem:" | awk '{printf "%.2f/%.2f MB (%.2f%%)", $3/1024, $2/1024, $3/$2 * 100}')
+
+# 获取虚拟内存使用情况
+swap_memory=$(free -m | grep "Swap:" | awk '{printf "%.2f/%.2f MB (%.2f%%)", $3/1024, $2/1024, $3/$2 * 100}')
+
+# 获取磁盘使用情况
+disk_usage=$(df -h / | awk '$NF=="/"{printf "%s/%s (%s)", $3, $2, $5}')
+
+# 获取公网IPv4地址
+public_ipv4=$(curl -s https://httpbin.org/ip | grep -oP '(?<=origin": ")[^"]+')
+
+# 获取公网IPv6地址
+interface="eth0"  # 指定您要获取IPv6地址的网络接口
+local_ipv6=$(ip -6 addr show dev $interface | grep -oP '(?<=inet6\s)[\da-fA-F:]+')
+
+# 获取地理位置
+location=$(curl -s ipapi.com/json | awk -F',' '{print $6}')
+
+# 获取系统时间
+system_time=$(date +"%Y-%m-%d %I:%M %p")
+
+# 显示系统信息
+echo "系统信息:"
+echo "---------------------------"
 echo "主机名: $hostname"
-echo "内核版本: $kernel_version"
-echo "操作系统: $os"
-echo "运行时间: $uptime"
-echo "CPU: $cpu"
-echo "内存: $memory"
-echo "磁盘空间: $disk"
-echo "-------------------------------------"
+echo "运营商: $isp"
+echo "Linux版本: $linux_version"
+echo "CPU架构: $cpu_arch"
+echo "CPU型号: $cpu_model"
+echo "CPU核心数: $cpu_cores"
+echo "CPU占用: $cpu_usage%"
+echo "物理内存: $physical_memory"
+echo "虚拟内存: $swap_memory"
+echo "硬盘占用: $disk_usage"
+echo "公网IPv4地址: $public_ipv4"
+echo "IPv6地址: $local_ipv6"
+echo "地理位置: $location"
+echo "系统时间: $system_time"
+echo "---------------------------"
+
+# 结束脚本
+exit 0
