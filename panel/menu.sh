d380586dc70fb530f7f711c50d287000527d90c8
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2024-01-02 17:38:31 +0800

    Update menu.sh

--- panel/menu.sh
+++ panel/menu.sh
@@ -8,32 +8,27 @@ repo_url="https://raw.githubusercontent.com/WJQSERVER/tools-dev/develop/"
 
 # 定义函数来执行选项1的操作
 option1() {
-    echo "执行选项1的操作"
-    # 在这里执行选项1的操作
+wget -O bt-install.sh ${repo_url}panel/bt-install.sh && chmod +x bt-install.sh && ./bt-install.sh
 }
 
 # 定义函数来执行选项2的操作
 option2() {
-    echo "执行选项2的操作"
-    # 在这里执行选项2的操作
+wget -O 1panel-install.sh ${repo_url}panel/1panel-install.sh && chmod +x 1panel-install.sh && ./1panel-install.sh
 }
 
 # 定义函数来执行选项3的操作
 option3() {
-    echo "执行选项3的操作"
-    # 在这里执行选项3的操作
+wget -O serverstatus.sh ${repo_url}panel/serverstatus.sh && chmod +x serverstatus.sh && ./serverstatus.sh
 }
 
-# 主循环
-while true; do
     # 清空屏幕
     clear
 
     # 显示菜单
     echo "请选择一个选项："
-    echo "1. 选项1"
-    echo "2. 选项2"
-    echo "3. 选项3"
+    echo "1. 宝塔面板"
+    echo "2. 1panel面板"
+    echo "3. ServerStatus面板/探针"
     echo "4. 退出"
 
     # 提示用户输入选项编号
