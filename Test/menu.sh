4de50867faa2244e20bb7c68f6567ac083998a58
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2024-01-02 16:59:49 +0800

    Update menu.sh

--- Test/menu.sh
+++ Test/menu.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-######################################
-# 简单菜单选择器脚本
-# 提供一个交互式菜单，根据用户选择执行相应操作
-######################################
-
 # 清空屏幕
     clear
 
 #常量
 repo_url="https://raw.githubusercontent.com/WJQSERVER/tools-dev/develop/"
 
+# 定义函数来执行选项1的操作
+option1() {
+    echo "三网回程测试"
+    wget -O 3mtr-back.sh ${repo_url}Test/3mtr-back.sh && chmod +x 3mtr-back.sh && ./3mtr-back.sh
+}
 
-# 显示菜单选项
-echo "欢迎使用简单菜单选择器脚本"
-echo "请选择以下选项："
+# 定义函数来执行选项2的操作
+option2() {
+    echo "开始三网测速"
+    wget -O 3speedtest.sh ${repo_url}Test/3speedtest.sh && chmod +x 3speedtest.sh && ./3speedtest.sh
+}
 
-# 显示菜单循环
+# 定义函数来执行选项3的操作
+option3() {
+    echo "正在返回主菜单"
+    # 在这里执行选项3的操作
+}
+
+# 主循环
 while true; do
+    # 显示菜单
+    echo "请选择一个选项："
     echo "1. 三网回程测试"
-    echo "2. 三网测速"
-    echo "3. 主菜单"
+    echo "2. 开始三网测速"
+    echo "3. 返回主菜单"
     echo "4. 退出"
 
-    # 获取用户输入
-    read -p "请输入选项编号: " choice
+    # 读取用户输入
+    read choice
 
-    # 根据用户选择执行相应操作
+    # 根据用户输入执行相应的操作
     case $choice in
         1)
-            echo "三网回程测试"
-            wget -O 3mtr-back.sh ${repo_url}Test/3mtr-back.sh && chmod +x 3mtr-back.sh && ./3mtr-back.sh
-            fi
+            option1
             ;;
         2)
-            echo "开始三网测速"
-            wget -O 3speedtest.sh ${repo_url}Test/3speedtest.sh && chmod +x 3speedtest.sh && ./3speedtest.sh
-            fi
+            option2
             ;;
         3)
-            echo "正在返回主菜单"
-            # 在这里执行选项3的操作
+            option3
             ;;
         4)
             echo "再见！"
@@ -51,6 +56,6 @@ while true; do
             ;;
     esac
 
-    # 清空屏幕
-    clear
+    # 执行完选项内的代码后，程序将继续执行下一条语句而不会进入循环
+    echo "选项执行完毕"
 done
