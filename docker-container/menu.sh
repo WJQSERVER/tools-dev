81dd331f3fff9788d3f3e44885edecfdb4727b8e
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2024-01-02 17:57:31 +0800

    Update menu.sh

--- docker-container/menu.sh
+++ docker-container/menu.sh
@@ -6,30 +6,39 @@ clear
 # 常量定义
 repo_url="https://raw.githubusercontent.com/WJQSERVER/tools-dev/develop/"
 
-# 定义函数来执行选项1的操作
-option1() {
-    echo "执行选项1的操作"
-    # 在这里执行选项1的操作
+# nginx proxy manager
+op1() {
+wget -O nginxproxymanager.sh ${repo_url}docker-container/nginxproxymanager.sh && chmod +x nginxproxymanager.sh && ./nginxproxymanager.sh
 }
 
-# 定义函数来执行选项2的操作
-option2() {
-    echo "执行选项2的操作"
-    # 在这里执行选项2的操作
+# portainer
+op2() {
+wget -O portainer.sh ${repo_url}docker-container/portainer.sh && chmod +x portainer.sh && ./portainer.sh
 }
 
-# 定义函数来执行选项3的操作
-option3() {
-    echo "执行选项3的操作"
-    # 在这里执行选项3的操作
+# speedtest-x
+op3() {
+wget -O speedtest-x.sh ${repo_url}docker-container/speedtest-x.sh && chmod +x speedtest-x.sh && ./speedtest-x.sh
+}
+
+#qinglong
+op4(){
+wget -O qinglong.sh ${repo_url}docker-container/qinglong.sh && chmod +x qinglong.sh && ./qinglong.sh
+}
+
+#uptime-kuma
+op5(){
+wget -O uptime-kuma.sh ${repo_url}docker-container/uptime-kuma.sh && chmod +x uptime-kuma.sh && ./uptime-kuma.sh
 }
 
 # 显示菜单
 echo "请选择一个选项："
-echo "1. 选项1"
-echo "2. 选项2"
-echo "3. 选项3"
-echo "4. 退出"
+echo "1. Nginx Proxy Manager 可视化Nginx管理面板"
+echo "2. Portainer 可视化Docker管理面板"
+echo "3. Speedtest-X"
+echo "4. 青龙面板"
+echo "5. Uptime Kuma"
+echo "0. 退出"
 
 # 提示用户输入选项编号
 echo -n "请输入选项对应的编号: "
@@ -38,15 +47,21 @@ read choice
 # 根据用户输入执行相应的操作
 case $choice in
     1)
-        option1
+        op1
         ;;
     2)
-        option2
+        op2
         ;;
     3)
-        option3
+        op3
         ;;
     4)
+        op4
+        ;;
+    5)
+        op5
+        ;;    
+    0)
         echo "再见！"
         exit
         ;;
