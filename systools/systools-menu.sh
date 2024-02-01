f52c06e2abac55509b755f61efbdf8684ac877de
WJQSERVER <wjqserver@outlook.com>
2024-02-01 19:43:05 +0800

    0.4

--- systools/systools-menu.sh
+++ systools/systools-menu.sh
@@ -181,29 +181,59 @@ function timezone(){
 wget -O timezone.sh ${repo_url}systools/timezone.sh && chmod +x timezone.sh && ./timezone.sh
 }
 
-#
-function (){
-
+#IPv4/6优先级切换
+function ipv(){
+wget -O ipv-switch.sh ${repo_url}systools/ipv-switch.sh && chmod +x ipv-switch.sh && ./ipv-switch.sh
 }
 
-#
-function (){
-
+#网络侧信息查看
+function networkinfo(){
+wget -O networkinfo.sh ${repo_url}systools/networkinfo.sh && chmod +x networkinfo.sh && ./networkinfo.sh
 }
 
-#
-function (){
-
+#修改DNS
+function change_dns(){
+wget -O change_dns.sh ${repo_url}systools/change_dns.sh && chmod +x change_dns.sh && ./change_dns.sh
 }
 
-#
-function (){
-
+#BBR管理面板
+function bbr-manager(){
+wget -O bbr-manager.sh ${repo_url}systools/bbr-manager.sh && chmod +x bbr-manager.sh && ./bbr-manager.sh
 }
 
-#
-function (){
+#端口占用检测
+function check_port_usage(){
+wget -O check_port_usage.sh ${repo_url}systools/check_port_usage.sh && chmod +x check_port_usage.sh && ./check_port_usage.sh
+}
 
+#UFW防火墙
+function ufw(){
+wget -O ufw.sh ${repo_url}systools/ufw.sh && chmod +x ufw.sh && ./ufw.sh    
+}
+
+#SWAP虚拟内存设置
+function swap(){
+wget -O swap.sh ${repo_url}systools/swap.sh && chmod +x swap.sh && ./swap.sh    
+}
+
+#用户管理
+function user_management(){
+wget -O user_management.sh ${repo_url}systools/user_management.sh && chmod +x user_management.sh && ./user_management.sh
+}
+
+#修改root密码
+function change-root-password(){
+wget -O change-root-password.sh ${repo_url}systools/change-root-password.sh && chmod +x change-root-password.sh && ./change-root-password.sh
+}
+
+#创建普通用户
+function create_user(){
+wget -O create_user.sh ${repo_url}systools/create_user.sh && chmod +x create_user.sh && ./create_user.sh
+}
+
+#生成强密码
+function gennerate_strong_password(){
+wget -O generate_strong_password.sh ${repo_url}systools/generate_strong_password.sh && chmod +x generate_strong_password.sh && ./generate_strong_password.sh    
 }
 
 #主菜单
