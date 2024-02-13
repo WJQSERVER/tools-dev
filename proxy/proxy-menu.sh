b8741251c05acf57ce680deb9fa6903f412744af
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2024-02-13 17:44:08 +0800

    hotfix

--- proxy/proxy-menu.sh
+++ proxy/proxy-menu.sh
@@ -64,7 +64,7 @@ function x-ui_f(){
 bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/x-ui/master/install.sh) 0.3.4.4
 }
 
-#X-UI_勇哥精简版(此版本有争议)
+#X-UI_勇哥精简版(此版本有争议)(闭源加密)(有挖矿嫌疑)
 function x-ui_yg(){
 wget -N https://gitlab.com/rwkgyg/x-ui-yg/raw/main/install.sh && bash install.sh
 }
