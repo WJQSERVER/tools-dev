91186f18a4a8cc19031bde45b2274fdaeeb2cbfb
WJQSERVER <wjqserver@outlook.com>
2024-02-17 17:21:16 +0800

    创建 koishi.sh

---
+++ program/koishi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+
+# 检查是否已经安装 Docker 和 Docker Compose
+if ! command -v docker >/dev/null || ! command -v docker-compose >/dev/null; then
+    echo "请先安装 Docker 和 Docker Compose。"
+    exit 1
+fi
+
+# 创建目录
+mkdir -p /root/data/docker_data/koishi
+cd /root/data/docker_data/koishi
+
+# 从用户输入中获取容器端口
+read -p "请输入容器端口: " PORT
+
+# 使用Docker-Cli
+docker run -p $PORT:5140 koishijs/koishi -v /root/data/docker_data/koishi:/koishi
+
+# 提示服务访问地址
+echo "服务已成功启动！"
+echo "请访问以下地址来访问您的服务："
+echo "http://<服务器IP>:$PORT"
