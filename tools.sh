9cf286afc6046d6d8303b19f11b3ff5fb6ee41de
WJQSERVER <wjqserver@outlook.com>
2024-03-06 22:58:57 +0800

    0.5

--- tools.sh
+++ tools.sh
@@ -12,13 +12,7 @@ sleep 1
 
 #BETA版
 repo_url="https://raw.githubusercontent.com/WJQSERVER/tools-dev/BETA/"
-
-# 检查配置文件是否存在
-conf_file="repo_url.conf"
-if [ ! -f "$conf_file" ]; then
-    # 创建配置文件并写入变量
-    echo "repo_url=$repo_url" > "$conf_file"
-fi
+echo "repo_url=$repo_url" > "$conf_file"
 
 # 导入配置文件
 source "repo_url.conf"
