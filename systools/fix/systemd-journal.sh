16115a31a899366289a3ca802380590acbf13595
WJQSERVER <wjqserver@outlook.com>
2024-03-14 07:59:26 +0800

    0.5

--- systools/fix/systemd-journal.sh
+++ systools/fix/systemd-journal.sh
@@ -4,3 +4,4 @@
 
 sudo mkdir -p /var/log/journal && sudo systemctl restart systemd-journald && sudo journalctl --flush
 grep -q "^Storage=" /etc/systemd/journald.conf && sudo sed -i 's/^Storage=.*/Storage=persistent/' /etc/systemd/journald.conf || echo "Storage=persistent" | sudo tee -a /etc/systemd/journald.conf > /dev/null && sudo systemctl restart systemd-journald && sudo journalctl --flush
+grep -q "^Storage=" /etc/systemd/journald.conf && sudo sed -i 's/^Storage=.*/Storage=none/' /etc/systemd/journald.conf || echo "Storage=none" | sudo tee -a /etc/systemd/journald.conf > /dev/null && sudo systemctl restart systemd-journald
